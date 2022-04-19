8e69ff75923200a2c40f3cc74217ae988f59423f
Ahn YoungSeon <dudtjs0920@naver.com>
2022-04-19 18:40:55 +0900

    CROF-12 git init

---
+++ src/main/java/com/sungshin/croffle/CroffleExpeditionApplication.java
@@ -0,0 +1,13 @@
+package com.sungshin.croffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroffleExpeditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroffleExpeditionApplication.class, args);
+	}
+
+}
