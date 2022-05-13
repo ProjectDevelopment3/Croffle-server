ea0e7def7402537b2b8d2c729349138164e8cce4
Ahn YoungSeon <dudtjs0920@naver.com>
2022-05-13 16:24:24 +0900

    [add] CROF-91 LikedCafe entity 생성

---
+++ src/main/java/com/sungshin/croffle/domain/LikedCafe.java
@@ -0,0 +1,27 @@
+package com.sungshin.croffle.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class LikedCafe {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long cafe_id;
+
+    @Column(nullable = false)
+    private Long user_id;
+
+    @Builder
+    public LikedCafe(Long cafe_id, Long user_id) {
+        this.cafe_id = cafe_id;
+        this.user_id = user_id;
+    }
+}
