202f18a97be871c5d4975041d58c1cf6de7c92f5
Ahn YoungSeon <dudtjs0920@naver.com>
2022-05-13 18:09:29 +0900

    [test] CROF-74 testcode 추가

---
+++ src/test/java/com/sungshin/croffle/service/CafeServiceTest.java
@@ -0,0 +1,52 @@
+package com.sungshin.croffle.service;
+
+import com.sungshin.croffle.domain.Cafe;
+import com.sungshin.croffle.domain.LikedCafe;
+import com.sungshin.croffle.domain.jpa.CafeRepository;
+import com.sungshin.croffle.domain.jpa.LikedCafeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class CafeServiceTest {
+
+    @Autowired
+    CafeService cafeService;
+    @Autowired
+    CafeRepository cafeRepository;
+    @Autowired
+    LikedCafeRepository likedCafeRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        Cafe cafe = Cafe.builder()
+                .name("테스트카페")
+                .coords("10,10")
+                .addr("서울시 강남구")
+                .build();
+        cafeRepository.save(cafe);
+    }
+
+    @Test
+    public void 카페스크랩추가() throws Exception {
+        // given
+        Cafe cafe = cafeRepository.findByName("테스트카페").get();
+
+        // when
+        Long likedCafeid = cafeService.likedCafeAdd(cafe.getId());
+
+        // then
+        LikedCafe likedCafe = likedCafeRepository.findById(likedCafeid).get();
+
+        assertThat(likedCafeid).isEqualTo(likedCafe.getId());
+
+    }
+}
