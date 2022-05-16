88f7fe154adcf5316406c03d8052857bcfb95932
juu924 <osj8528@gmail.com>
2022-05-17 02:13:52 +0900

    [fix](#CROF-86) 게시판 CRUD PR request 반영

---
+++ CroffleExpeditionApplication.java
@@ -0,0 +1,17 @@
+package com.sungshin.croffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class CroffleExpeditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroffleExpeditionApplication.class, args);
+	}
+
+}
