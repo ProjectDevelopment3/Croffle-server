416adf79a0c2c8d69eb3746e723763ceef0b6eb1
juu924 <osj8528@gmail.com>
2022-05-20 00:54:00 +0900

    delete folder

--- domain/jpa/MenuRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sungshin.croffle.domain.jpa;
-
-import com.sungshin.croffle.domain.Menu;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MenuRepository  extends JpaRepository<Menu,Long> {
-}
--- domain/jpa/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sungshin.croffle.domain.jpa;
-
-import com.sungshin.croffle.domain.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
