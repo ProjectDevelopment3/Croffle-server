f1ec5c7fdaaf29be300254aec0fb03c514bf0d46
Ahn YoungSeon <dudtjs0920@naver.com>
2022-05-22 21:41:09 +0900

    [add] CROF-33 user entity 추가

---
+++ src/main/java/com/sungshin/croffle/domain/User.java
@@ -0,0 +1,41 @@
+package com.sungshin.croffle.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long naverId;
+
+    @Column(length = 20)
+    private String nickname;
+
+    @Column(length = 20)
+    private String phone;
+
+    private String profileUrl;
+
+    @ColumnDefault("0")
+    private int owner;
+
+    @Builder
+    public User(Long id, Long naverId, String nickname, String phone, String profileUrl, int owner) {
+        this.id = id;
+        this.naverId = naverId;
+        this.nickname = nickname;
+        this.phone = phone;
+        this.profileUrl = profileUrl;
+        this.owner = owner;
+    }
+}
