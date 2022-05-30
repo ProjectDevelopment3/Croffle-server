cbc7efeb57bf1917d7a7e1e61ab7252b56586dc4
Ahn YoungSeon <dudtjs0920@naver.com>
2022-05-30 23:31:03 +0900

    [add] CROF-100 dto 기본 생성자 추가

--- src/main/java/com/sungshin/croffle/dto/stamp/AddStampRequestDto.java
+++ src/main/java/com/sungshin/croffle/dto/stamp/AddStampRequestDto.java
@@ -2,8 +2,10 @@ package com.sungshin.croffle.dto.stamp;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class AddStampRequestDto {
     private Long cafeId;
     private Long userId;
