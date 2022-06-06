953ad25636134b36e53b68dcdaabb56c5d1f607b
Ahn YoungSeon <dudtjs0920@naver.com>
2022-06-07 01:10:03 +0900

    [del] 필요 없는 코드 삭제WebConfig

--- src/main/java/com/sungshin/croffle/config/WebConfig.java
+++ src/main/java/com/sungshin/croffle/config/WebConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://34.64.45.86")
-                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "DELETE", "PUT")
-                .maxAge(3600);
-    }
-}
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "http://34.64.45.86")
+//                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "DELETE", "PUT")
+//                .maxAge(3600);
+//    }
+//}
