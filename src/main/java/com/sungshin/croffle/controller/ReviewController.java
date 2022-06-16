511e3469aa6d427c9794aff53e44c2a5a8d64918
juu924 <osj8528@gmail.com>
2022-06-16 23:03:31 +0900

    (#CROF-133) [fix] 리뷰 조회 Generic 타입 수정

--- src/main/java/com/sungshin/croffle/controller/ReviewController.java
+++ src/main/java/com/sungshin/croffle/controller/ReviewController.java
@@ -3,12 +3,13 @@ package com.sungshin.croffle.controller;
 import com.sungshin.croffle.config.auth.UserPrincipal;
 import com.sungshin.croffle.dto.Response;
 import com.sungshin.croffle.dto.review.ReviewRequestDto;
+import com.sungshin.croffle.dto.review.SearchReviewDto;
 import com.sungshin.croffle.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,10 +28,20 @@ public class ReviewController {
 
     @GetMapping("/review/{cafe_id}")
     public Response searchReview(@PathVariable Long cafe_id){
-        return Response.builder()
+        List<SearchReviewDto> reviewList = reviewService.searchReview(cafe_id);
+        
+        if(reviewList.size() == 0){
+            return Response.<SearchReviewDto>builder()
+                    .code("200")
+                    .messages("일치하는 리뷰가 없습니다.")
+                    .data(reviewList)
+                    .build();
+        }
+
+        return Response.<SearchReviewDto>builder()
                 .code("200")
                 .messages("리뷰 조회에 성공하였습니다.")
-                .data(Collections.singletonList(reviewService.searchReview(cafe_id)))
+                .data(reviewList)
                 .build();
 
     }
