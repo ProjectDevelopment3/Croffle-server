8dc10523e072cc4886cec827ea73732d55f90308
Ahn YoungSeon <dudtjs0920@naver.com>
2022-06-16 20:12:59 +0900

    [test] CROF-148 게시글 상세조회 테스트 추가

--- src/test/java/com/sungshin/croffle/service/BoardServiceTest.java
+++ src/test/java/com/sungshin/croffle/service/BoardServiceTest.java
@@ -3,12 +3,18 @@ package com.sungshin.croffle.service;
 import com.sungshin.croffle.domain.board.Board;
 import com.sungshin.croffle.domain.board.BoardCategory;
 import com.sungshin.croffle.domain.jpa.BoardRepository;
+import com.sungshin.croffle.domain.jpa.UserRepository;
+import com.sungshin.croffle.domain.user.Role;
+import com.sungshin.croffle.domain.user.User;
 import com.sungshin.croffle.dto.board.BoardRequestDto;
+import com.sungshin.croffle.dto.board.BoardSearchWrapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -18,6 +24,10 @@ public class BoardServiceTest {
     BoardService boardService;
     @Autowired
     BoardRepository boardRepository;
+    @Autowired
+    UserRepository userRepository;
+    Long userId;
+    Long boardId;
 
     @BeforeEach
     public void beforeEach() {
@@ -26,12 +36,29 @@ public class BoardServiceTest {
                 .content("테스트 본문")
                 .boardCategory(BoardCategory.TALK)
                 .build();
-        boardRequestDto.setUserId(1L);
-        boardService.savePost(boardRequestDto);
+        User user = User.builder()
+                .name("testuser")
+                .naverId("testtesttesttest")
+                .nickname("testnickname")
+                .role(Role.USER)
+                .build();
+        user = userRepository.save(user);
+        boardRequestDto.setUserId(user.getId());
+        userId = user.getId();
+        boardId = boardService.savePost(boardRequestDto);
     }
 
     @Test
-    public void 게시판글상세조회() {
+    public void 게시글상세조회() {
+        // given
 
+        // when
+        BoardSearchWrapper board = boardService.getPost(boardId);
+
+        // then
+        assertThat(board.getTitle()).isEqualTo("테스트 제목");
+        assertThat(board.getCategory()).isEqualTo(BoardCategory.TALK.toString());
+        assertThat(board.getContent()).isEqualTo("테스트 본문");
+        assertThat(board.getNickname()).isEqualTo("testnickname");
     }
 }
