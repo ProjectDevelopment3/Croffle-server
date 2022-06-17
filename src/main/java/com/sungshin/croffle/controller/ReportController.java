cd4292a3fba1203262896134d5af36e6ed8f9a7d
AYoungSn <dudtjs0920@naver.com>
2022-06-17 16:26:23 +0900

    [feat] report 기능 파일 삭제되서 다시 추가

---
+++ src/main/java/com/sungshin/croffle/controller/ReportController.java
@@ -0,0 +1,45 @@
+package com.sungshin.croffle.controller;
+
+import com.sungshin.croffle.config.auth.UserPrincipal;
+import com.sungshin.croffle.dto.Response;
+import com.sungshin.croffle.dto.report.InfoReportDto;
+import com.sungshin.croffle.dto.report.ReportCafeDto;
+import com.sungshin.croffle.service.ReportService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @PostMapping("/report/info")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public Response info(Authentication authentication,
+                         @RequestBody InfoReportDto reportDto) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        reportService.saveInfo(reportDto, userPrincipal.getId());
+        return Response.builder()
+                .code("201")
+                .messages("오류 제보하기에 성공하였습니다.")
+                .build();
+    }
+
+    @PostMapping("/report/menu")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public Response menu(Authentication authentication,
+                         @RequestBody ReportCafeDto reportCafeDto) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        reportService.saveMenu(reportCafeDto);
+        return Response.builder()
+                .code("201")
+                .messages("메뉴 제보하기에 성공하였습니다.")
+                .build();
+    }
+
+}
---
+++ src/main/java/com/sungshin/croffle/dto/report/ReportCafeDto.java
@@ -0,0 +1,33 @@
+package com.sungshin.croffle.dto.report;
+
+import com.sungshin.croffle.domain.Cafe;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class ReportCafeDto {
+    private String cafeName;
+    private String roadAddress;
+    private List<ReportMenuDto> menuList;
+
+
+    public Cafe toEntity(){
+        Cafe build = Cafe.builder()
+                .name(cafeName)
+                .addr(roadAddress)
+                .build();
+        return build;
+    }
+
+    @Builder
+    public ReportCafeDto(String cafeName, String roadAddress, List<ReportMenuDto> menuList){
+        this.cafeName = cafeName;
+        this.roadAddress = roadAddress;
+        this.menuList = menuList;
+    }
+
+}
