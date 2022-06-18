2060b763025b2c6b11dbbd5492cbd83f9520b04e
juu924 <osj8528@gmail.com>
2022-06-18 21:55:07 +0900

    (#CROF-173) [feat] 유저 전화번호로 조회하는 기능 추가

--- src/main/java/com/sungshin/croffle/domain/jpa/UserRepository.java
+++ src/main/java/com/sungshin/croffle/domain/jpa/UserRepository.java
@@ -1,7 +1,11 @@
 package com.sungshin.croffle.domain.jpa;
 
 import com.sungshin.croffle.domain.user.User;
+import com.sungshin.croffle.dto.user.StampUserInfoDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -9,4 +13,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByNaverId(String naverId);
 
     Optional<User> findByName(String name);
+
+    Optional<User> findByPhone(String phone);
+
+    @Query(value = "select u.id, u.name, s.stampCnt from user u" + " LEFT JOIN " + " stamp s" +
+            " on u.id = s.userId where u.id=:id and s.cafeId=:cafeId", nativeQuery = true)
+    StampUserInfoDto findUserById(@Param("id") Long id, @Param("cafeId")Long cafeId);
 }
---
+++ src/main/java/com/sungshin/croffle/dto/user/StampUserInfoDto.java
@@ -0,0 +1,6 @@
+package com.sungshin.croffle.dto.user;
+
+public interface StampUserInfoDto {
+    String getName();
+    int getstampCnt();
+}
