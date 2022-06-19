6b6c84146e12ee32d519fe6f3e1be56357cc1811
AYoungSn <dudtjs0920@naver.com>
2022-06-19 21:21:47 +0900

    [feat] WebConfig add

--- src/main/java/com/sungshin/croffle/config/WebConfig.java
+++ src/main/java/com/sungshin/croffle/config/WebConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000", "http://34.64.45.86")
-//                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "DELETE", "PUT")
-//                .maxAge(3600);
-//    }
-//}
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://34.64.45.86", "http://34.64.173.239")
+                .allowedMethods("OPTIONS", "HEAD", "GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
+                .maxAge(3600);
+    }
+}
