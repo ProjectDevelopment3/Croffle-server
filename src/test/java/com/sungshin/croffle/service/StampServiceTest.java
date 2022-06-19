d1841bdf4c263444f08b1b524fc1995455a91a62
AYoungSn <dudtjs0920@naver.com>
2022-06-19 11:32:30 +0900

    [test] stamp 조회 테스트

---
+++ src/test/java/com/sungshin/croffle/service/StampServiceTest.java
@@ -0,0 +1,40 @@
+package com.sungshin.croffle.service;
+
+import com.sungshin.croffle.domain.jpa.StampRepository;
+import com.sungshin.croffle.dto.stamp.StampListDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+public class StampServiceTest {
+
+    @Autowired
+    StampService stampService;
+    @Autowired
+    StampRepository stampRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+
+    }
+
+    @Test
+    void 스탬프_조회하기() {
+        // given
+        List<StampListDto> stampListDtos = stampRepository.findStampAndCafeByUserId(1L);
+        System.out.println(stampListDtos.size());
+        // when
+        int cnt = stampService.stampList(1L).size();
+
+        // then
+        assertThat(stampService.stampList(1L).size()).isEqualTo(stampListDtos.size());
+    }
+}
